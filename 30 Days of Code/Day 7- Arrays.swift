e9377136c047872802dcfa5c30d3c730be4e29ae
Van Le <van.le@edge-works.net>
2019-05-06 11:27:54 +0700

    30 Days of Code -> Day 7: Arrays

---
+++ 30 Days of Code/Day 7- Arrays.swift
@@ -0,0 +1,22 @@
+
+
+import Foundation
+
+guard let n = Int((readLine()?.trimmingCharacters(in: .whitespacesAndNewlines))!)
+    else { fatalError("Bad input") }
+
+guard let arrTemp = readLine() else { fatalError("Bad input") }
+let arr: [Int] = arrTemp.split(separator: " ").map {
+    if let arrItem = Int($0.trimmingCharacters(in: .whitespacesAndNewlines)) {
+        return arrItem
+    } else { fatalError("Bad input") }
+}
+
+guard arr.count == n else { fatalError("Bad input") }
+
+var arrResult = [String]()
+for i in arr.reversed() {
+    arrResult.append(String(i))
+}
+
+print(arrResult.joined(separator: " "))
